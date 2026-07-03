0557c98392bd44250297d3180417b4f84c3fe465
Dimitar <dimitardavidov5@gmail.com>
2026-07-04 02:04:32 +0300

    [UPDATED] styling on navbar and new background image

--- src/App/ClientApp/src/components/Navbar/Navbar.tsx
+++ src/App/ClientApp/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import { logout } from "../../features/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { capitalizeFirstLetter } from "../../utils/formatDisplayName";
 import "./Navbar.scss";
 
 function Navbar() {
@@ -12,7 +13,8 @@ function Navbar() {
   );
   const isLoggedIn = !!accessToken;
   const isAdmin = role === "Admin";
-  const displayName = username ?? "User";
+  const displayName = capitalizeFirstLetter(username ?? "User");
+  const userInitial = displayName.charAt(0).toUpperCase();
 
   function handleLogout() {
     dispatch(logout());
@@ -29,28 +31,34 @@ function Navbar() {
         {isLoggedIn ? (
           <div className="navbar-user-menu">
             <button type="button" className="navbar-user-trigger">
-              {displayName}
+              <span className="navbar-user-avatar" aria-hidden="true">
+                {userInitial}
+              </span>
+              <span className="navbar-user-name">{displayName}</span>
+              <span className="navbar-user-chevron" aria-hidden="true" />
             </button>
 
             <div className="navbar-dropdown">
-              {isAdmin && (
-                <Link to="/admin" className="navbar-dropdown-item">
-                  Admin Panel
+              <div className="navbar-dropdown-panel">
+                {isAdmin && (
+                  <Link to="/admin" className="navbar-dropdown-item">
+                    Admin Panel
+                  </Link>
+                )}
+                <Link to="/account" className="navbar-dropdown-item">
+                  Account
                 </Link>
-              )}
-              <Link to="/account" className="navbar-dropdown-item">
-                Account
-              </Link>
-              <Link to="/appointments" className="navbar-dropdown-item">
-                Appointments
-              </Link>
-              <button
-                type="button"
-                className="navbar-dropdown-logout"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
+                <Link to="/appointments" className="navbar-dropdown-item">
+                  Appointments
+                </Link>
+                <button
+                  type="button"
+                  className="navbar-dropdown-logout"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </div>
             </div>
           </div>
         ) : (
